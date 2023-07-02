56a39b3a251735c4529546673a4afc9adf8a9897
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-02 21:52:00 +0530

    Add files via upload

---
+++ basicprograms/Sumnnums.java
@@ -0,0 +1,9 @@
+public class Sumnnums {
+    public static void main(String[] args) {
+        int n = 56;
+        int sum = 0;
+        for(int i =0;i<=n;i++)
+            sum+=i;
+        System.out.println(sum);
+    }
+}
