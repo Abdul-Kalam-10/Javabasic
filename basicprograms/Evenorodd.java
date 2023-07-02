71c72a455f9ee159f790e5e3001916c12e89f937
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-02 09:42:39 +0530

    Add files via upload

---
+++ basicprograms/Evenorodd.java
@@ -0,0 +1,10 @@
+public class Evenorodd {
+    public static void main(String[] args) {
+        int num = 23;
+        if(num%2==0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+
+    }
+}
---
+++ basicprograms/Greatestofthree.java
@@ -0,0 +1,13 @@
+public class Greatestofthree {
+    public static void main(String[] args) {
+        int n1 = 24;
+        int n2 = 34;
+        int n3 = 14;
+        if(n1>n2)
+            System.out.println("n1 is greater");
+        else if(n1<n2)
+            System.out.println("n2 is greater");
+        else
+            System.out.println("n3 is greater");
+    }
+}
