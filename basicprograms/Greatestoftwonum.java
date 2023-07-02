b4d1f4bf7a687da84321b0db42dc77d7b2d3d75b
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-02 09:44:50 +0530

    Add files via upload

---
+++ basicprograms/Greatestoftwonum.java
@@ -0,0 +1,13 @@
+public class Greatestoftwonum {
+    public static void main(String[] args) {
+        int n1 = 34;
+        int n2 = 45;
+        if(n1 == n2)
+            System.out.println("both are equal");
+        else if (n1 > n2)
+            System.out.println("number 1 is greater");
+        else
+            System.out.println("number 2 is greater");
+        }
+    }
+
