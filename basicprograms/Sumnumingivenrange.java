bdaaa58e7bda96c91b7c8f3e08bd8d542ad67a76
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-03 13:11:56 +0530

    Add files via upload

---
+++ basicprograms/Sumnumingivenrange.java
@@ -0,0 +1,11 @@
+public class Sumnumingivenrange {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 15;
+        int sum = 0;
+        for(int i=a;i<=b;i++)
+            sum+=i;
+        System.out.println(sum);
+    }
+}
+
