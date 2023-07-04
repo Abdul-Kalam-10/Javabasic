a41a27cc9fcd912b8c39b67c36721aeaa1f89626
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-04 17:42:20 +0530

    Add files via upload

---
+++ basicprograms/Squarenumbers.java
@@ -0,0 +1,10 @@
+public class Squarenumbers {
+    public static void main(String[] args) {
+            int i =1 ;
+            while(i<5){
+                System.out.println(Math.pow(i,i));
+                i++;
+            }
+        }
+    }
+
