7ce3c60c47c19c67347c58e5696c0a6dac01fa16
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-06 09:04:22 +0530

    Add files via upload

---
+++ basicprograms/Leapyear.java
@@ -0,0 +1,12 @@
+public class Leapyear {
+    public static void main(String[] args) {
+    int year = 2023;
+    if(year % 400 == 0)
+        System.out.println(year + "is leap year");
+    else if(year %4==0 && year %100 !=0)
+        System.out.println(year +"is leap year");
+    else
+        System.out.println("not a leap year");
+    }
+
+}
