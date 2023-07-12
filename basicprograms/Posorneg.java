6a1397e902f31bab6212971104883cd99b9e83af
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-12 09:30:43 +0530

    Add files via upload

---
+++ basicprograms/Posorneg.java
@@ -0,0 +1,12 @@
+public class Posorneg {
+    public static void main(String[] args) {
+        int num = 0;
+        if (num>0)
+            System.out.println("number is positive");
+        else if (num<0)
+            System.out.println("number is negative");
+        else
+            System.out.println("zero");
+    }
+}
+
