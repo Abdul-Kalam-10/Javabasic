a273fa8e350ef17b1ea604c7ef66ccbffbe4f07e
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-21 17:49:38 +0530

    Add files via upload

---
+++ Arrays/Printarray.java
@@ -0,0 +1,8 @@
+public class Printarray {
+    public static void main(String[] args) {
+        int[] arr = {23,43,35,53,67};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+   }
+}
