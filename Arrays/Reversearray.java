dd09458b7c5362ed7da20522ec7ed489c2bec8a9
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-21 17:54:27 +0530

    Add files via upload

---
+++ Arrays/Reversearray.java
@@ -0,0 +1,11 @@
+public class Reversearray {
+    public static void main(String[] args) {
+        int[] arr = {23,54,23,35,63};
+        int i = arr.length-1;
+        while(i>=0){
+            System.out.println(arr[i]);
+            i=i-1;
+        }
+    }
+}
+
