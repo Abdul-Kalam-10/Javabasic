c798a9b6c8de216491e5f62de9c24f76eb704645
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-07-27 19:45:09 +0530

    Add files via upload

---
+++ Arrays/Negativeelementinarray.java
@@ -0,0 +1,10 @@
+public class Negativeelementinarray {
+    public static void main(String[] args) {
+        int[] arr = {10,-20,-30,40,-50};
+        for (int i=0;i< arr.length;i++){
+            if(arr[i]<0){
+                System.out.println(arr[i] );
+            }
+        }
+    }
+}
