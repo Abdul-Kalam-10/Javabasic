180396c946ceaf2d3eef3ac6b8bf7a9cdc613d9c
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-08-25 17:44:42 +0530

    Add files via upload

---
+++ Arrays/Removeduplicateinsortedarray.java
@@ -0,0 +1,17 @@
+public class Removeduplicateinsortedarray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,3,4,5,5};
+        int[] temp = new int[arr.length];
+        int j=0;
+        for(int i=0;i< arr.length-1;i++){
+            if (arr[i] != arr[i+1]){
+                temp[j] = arr[i];
+                j++;
+            }
+        }
+        temp[j]=arr[arr.length-1];
+        for(int i=0;i< arr.length;i++){
+            System.out.println(temp[i]+" ");
+        }
+    }
+}
